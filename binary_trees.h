a8f5d3f2f3ffe42dc5763bbad45c25c02703ca42
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-07-20 08:17:48 +0100

    quick fix

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREES_H
 #include <stddef.h>
 #include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -10,12 +11,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
+
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
